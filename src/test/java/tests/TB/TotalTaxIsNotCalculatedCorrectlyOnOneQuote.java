2fbd42227aaa012adf38e5fc98e8a5f8b3fa1364
mihailklimovich <mishaklimovich87@gmail.com>
2022-06-13 18:41:08 +0300

    Test commit32

--- src/test/java/tests/TB/TotalTaxIsNotCalculatedCorrectlyOnOneQuote.java
+++ src/test/java/tests/TB/TotalTaxIsNotCalculatedCorrectlyOnOneQuote.java
@@ -25,7 +25,7 @@ public class TotalTaxIsNotCalculatedCorrectlyOnOneQuote extends BaseTest {
             " included in the calculations.")
     @Severity(SeverityLevel.NORMAL)
     @Story("TB-277: Total tax is not calculated correctly on one Quote.")
-    @Xray(requirement = "TEST-1234", test = "TEST-1", labels = "core addition")
+    @Xray(requirement = "TTP-310", test = "TTP-310", labels = "core addition")
     public void case1() throws InterruptedException, IOException {
         loginPage.authoriseURL(SFDX, SFDX_AUTH_URL, ORG_USERNAME);
         myceQuotes.deleteQuoteSFDX(SFDX, "Name='TotalTaxIsNotCalculatedCorrectlyOnOneQuoteAutoTest'", ORG_USERNAME);
